4a0ad455aae424db9b251398a8494450ff8313e0
davidpauljones <davidpauljones@gmail.com>
2026-01-03 19:31:04 +1100

    Add config.js to repo for GitHub Pages deployment

---
+++ config.js
@@ -0,0 +1,13 @@
+// Firebase Configuration
+// This file contains sensitive configuration and should NOT be committed to git
+// Copy config.template.js to config.js and fill in your values
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBvHqOHcW--e76fVp7exthrsapUYYcwS3Q",
+    authDomain: "partysync-17cd5.firebaseapp.com",
+    projectId: "partysync-17cd5",
+    storageBucket: "partysync-17cd5.appspot.com",
+    messagingSenderId: "294351433712",
+    appId: "1:294351433712:web:826470510443b924a6b735",
+    measurementId: "G-PF73SF4VGY"
+};
